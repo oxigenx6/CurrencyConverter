88e6e83a73657c7e445ee4f174dd0c513c1b9b09
OXI <oxigex6@yandex.ru>
2019-04-22 18:30:58 +0700

    Добавлено * Основной функционал: конвертация, возможность переподключения, вывод результата * Сообщение об ошибке подкючения * Переподключение к серверу

--- app/src/main/java/com/DH/currencyconverter/MainActivity.java
+++ app/src/main/java/com/DH/currencyconverter/MainActivity.java
@@ -2,8 +2,13 @@ package com.DH.currencyconverter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.text.DecimalFormat;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -14,14 +19,20 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class MainActivity extends AppCompatActivity {
 
     private Double rub;
-    TextView res = findViewById(R.id.result);
-    TextView cur = findViewById(R.id.cur);
-    EditText num = findViewById(R.id.inputNum);
+    TextView res;
+    TextView cur;
+    EditText num;
+    Button errorButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        res = findViewById(R.id.result);
+        cur = findViewById(R.id.cur);
+        num = findViewById(R.id.inputNum);
+        errorButton = findViewById(R.id.errorButton);
 
         setupRetrofit();
     }
@@ -37,16 +48,51 @@ public class MainActivity extends AppCompatActivity {
         request.enqueue(new Callback<Currency>() {
             @Override
             public void onResponse(Call<Currency> call, Response<Currency> response) {
-                rub = response.body().rates.get("RUB");
-                cur.setText(Double.toString(rub));
+                //rub = response.body().rates.get("RUB");
+                cur.setText(
+                        new DecimalFormat("0.00").format(response.body().rates.get("RUB"))
+                );
+                rub = Double.parseDouble(cur.getText().toString());
+                errorButton.setVisibility(View.GONE);
             }
 
             @Override
             public void onFailure(Call<Currency> call, Throwable t) {
-
+                Toast.makeText(getApplicationContext(),
+                        R.string.error_message, Toast.LENGTH_SHORT).show();
+                errorButton.setVisibility(View.VISIBLE);
             }
         });
     }
 
-    void on
+    public void repeatConnectOnClick(View view) {
+        setupRetrofit();
+    }
+
+    public void eurToRubButtOnClick(View view) {
+        if (rub == null || num.getText().toString().matches("")) {
+            Toast.makeText(getApplicationContext(),
+                    R.string.error_message_cur, Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        res.setText(Double.toString(
+                Double.parseDouble(num.getText().toString().
+                        replace(",",".")) * rub
+        ) + " RUB");
+    }
+
+    public void rubToEurButtonOnClick(View view) {
+        if (rub == null || num.getText().toString().matches("")) {
+            Toast.makeText(getApplicationContext(),
+                    R.string.error_message_cur, Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        res.setText(
+                new DecimalFormat("0.000").format(
+                Double.parseDouble(num.getText().toString().
+                        replace(",",".")) / rub) + " EUR"
+        );
+    }
 }
